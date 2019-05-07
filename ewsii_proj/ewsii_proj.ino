78615c95f6001c38a5892cef89dff45bb814942e
amoghjoshi <amogh.joshi@somaiya.edu>
2019-05-07 16:47:25 +0530

    final commit

--- ewsii_proj/ewsii_proj.ino
+++ ewsii_proj/ewsii_proj.ino
@@ -11,9 +11,14 @@ void setup() {
   // put your setup code here, to run once:
   lcd.begin();
   lcd.noBacklight();
+  lcd.print("Please Wait");
+  lcd.setCursor(0,1);
+  lcd.print("Starting Up...");
+  delay(1000);
+  lcd.clear();
   lcd.print("Insert Sample");
   lcd.setCursor(0,1);
-  lcd.print("and press Start");
+  lcd.print("and Press Start");
   delay(1000);
   lcd.clear();
   pinMode(5,OUTPUT);
@@ -34,6 +39,7 @@ void loop()
   t=cs.getval(1,2);      
   a=log10(k*t);
   A=String(a,6);
+  lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Absorbance :");
   lcd.setCursor(0,1);
